b4a70b00be0072a123ce5e3c48ab9522058ccff9
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-01-06 10:51:43 +0200

    exit setup on failure to create/activate env

--- setup-env.sh
+++ setup-env.sh
@@ -6,8 +6,10 @@
 # create virtual environment
 VENV_NAME="$1"
 
-python -m venv "$VENV_NAME"
-source "$VENV_NAME"/bin/activate
+[ -d "$VENV_NAME" ] && echo "Virtual environment already exists" && exit 1
+
+python -m venv "$VENV_NAME" || { echo "Failed to create virtual environment" && exit 1; }
+source "$VENV_NAME"/bin/activate || { echo "Failed to activate virtual environment" && exit 1; }
 
 # install source code dependencies
 pip install pysindy numpy pyGPGO matplotlib scikit-learn scipy pathlib pandas cvxpy gurobipy
